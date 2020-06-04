81faf99e1655df6afb4b48496342ab95382860a5
Anh Viet <anhviet5121pc@gmail.com>
2020-06-04 11:04:10 +0700

    Server and database

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+// BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+//ROUTES
+
+app.use('/', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://anhviet5121pc:iotiuclub@cluster0-agrfs.mongodb.net/test?retryWrites=true&w=majority'
+}
