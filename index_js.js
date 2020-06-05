b81c598afe57a8778e01a3d9742876f1215ee31c
Tran Tan Tai <Kuerl14@gmail.com>
2020-06-05 16:46:45 +0700

    Kuerl - Update.Ver1

--- index_js.js
+++ index_js.js
@@ -1,51 +1,60 @@
-function errorfunction() {
-    alert("Sorry! This function will not work because i don't know how to do it :)")
+// Save password to cookie (Not Complete!)
+function savePwd2Cookie() {
+    alert("Task is not completed!");
 }
+//-------------------------------------------------------------------------------------------------
+// Forgot password (Not Complete!)
+function errorfunction() {
+    alert("Sorry! This function will not work because i don't know how to do it :)");
+}
+//-------------------------------------------------------------------------------------------------
+// Show the Password Function
+function showPwd() {
+    var pwd = document.getElementById("Password");
+    var pwdValue = pwd.value;
+    if (pwdValue == "") {
+        alert("Fill the Password!");
+    }
+    else {
+        changePwdType();
+    }
+function changePwdType() {
+    var pwd = document.getElementById("Password");
+    if (pwd.type == "password"){
+        pwd.type = "text"
+        var eyeIcon = document.getElementById("eyeIcon");
+        eyeIcon.src= "eye.png"
+    }
+    else {
+        pwd.type = "password"
+        var eyeIcon = document.getElementById("eyeIcon");
+        eyeIcon.src = "eye-slash.webp"
+    }
+}
+}
+//-------------------------------------------------------------------------------------------------
+// Check Login Element - Post to server
+function loginCheck() {
+    var username = document.getElementById("Username");
+    var usernameValue = username.value;
+    var pwd = document.getElementById("Password");
+    var pwdValue = pwd.value;
+    if (usernameValue == "" || pwdValue == "") {
+        showErrorMsg();
+    }
+    else {
+        request2Sver();
+    }
+}
+// Show the Err: Fill Us.name and Pwd both
 function showErrorMsg() {
     var x = document.getElementById("h6"); // h6 is
     if (x.style.display === "none") {
         x.style.display = "block";
     }
 }
-//
-function check() {
-    var us = document.getElementById("Username");
-    var user = us.value;
-    var pw = document.getElementById("Password");
-    var pwd = pw.value;
-    if (user == "" || pwd == "") {
-        showErrorMsg();
-    }
-    else {
-        goto();
-    }
-}
-// Save password to cookie
-function savePwd2Cookie() {
-    alert("Task is not completed!")
-}
-// eye
-function eyec() {
-    var v1 = document.getElementById("Password");
-    var v2 = v1.value;
-    if (v2 == "") {
-        alert("Fill the Password!");
-    }
-    else {
-        eyecc()
-    }
-}
-function eyecc() {
-    var v1 = document.getElementById("Password");
-    if (v1.type == "password") {
-        v1.type = "text"
-    }
-    else {
-        v1.type = "password"
-    }
-}
-//res
-function goto() {
+// Request to Server
+function request2Sver() {
     var usernameElement = document.getElementById("Username");
     var user = usernameElement.value;
     var pwdElement = document.getElementById("Password");
@@ -59,29 +68,19 @@ function goto() {
             console.log(response);
             handleLoginResponse(response);
         });
-    // gotoo();
 }
 
 function handleLoginResponse(response) {
     if (response && response.status == 200) { // successfully login
-        div3();
+        calcDiv();
     } else {
         alert("Login failed!");
     }
 }
-//res2
-function gotoo() {
-    if (this.status == 200) {
-        div3();
-    }
-    else {
-        alert("Check your elements again!" + this.status);
-    }
-}
-//div3
-function div3() {
+// Show the Calculator Div
+function calcDiv() {
     div1 = document.getElementById("login")
     div1.style.display = "none";
-    div3 = document.getElementById("calculator");
-    div3.style.display = "block";
+    calcDiv = document.getElementById("calculator");
+    calcDiv.style.display = "block";
 }
